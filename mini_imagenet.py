27776964b2962356538c857b2efc49983a267ff8
songrotek <songrotek@qq.com>
2018-11-10 10:25:36 +0800

    update

--- mini_imagenet.py
+++ mini_imagenet.py
@@ -33,7 +33,7 @@ class MiniImageNet(Dataset):
         self.label = label
         if is_train:
             self.transform = transforms.Compose([
-                transforms.RandomResizedCrop(84),
+                transforms.RandomResizedCrop(224),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
@@ -41,8 +41,8 @@ class MiniImageNet(Dataset):
             ])
         else:
             self.transform = transforms.Compose([
-                transforms.Resize(96),
-                transforms.CenterCrop(84),
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
