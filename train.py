a1bcf0776584a18d430e321b3db0033da4c39047
Flood Sung <floodsung@Floods-MacBook-Pro.local>
2019-01-31 14:57:02 +0800

    update

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from utils import pprint, set_gpu, ensure_path, Averager, Timer, count_acc, eucl
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--max-epoch', type=int, default=1000)
-    parser.add_argument('--save-epoch', type=int, default=20)
+    parser.add_argument('--save-epoch', type=int, default=100)
     parser.add_argument('--shot', type=int, default=1)
     parser.add_argument('--query', type=int, default=15)
     parser.add_argument('--train-way', type=int, default=30)
