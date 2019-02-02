a962a988750be298cc483e36e77fb7458457ad26
Flood Sung <floodsung@Floods-MacBook-Pro.local>
2019-02-02 11:19:57 +0800

    update

--- convnet.py
+++ convnet.py
@@ -30,9 +30,6 @@ class Convnet(nn.Module):
         feature2 = self.conv2(feature1)
         feature3 = self.conv3(feature2)
         feature4 = self.conv4(feature3)
-        return feature1.view(feature1.size(0), -1),
-               feature2.view(feature2.size(0), -1),
-               feature3.view(feature3.size(0), -1),
-               feature4.view(feature4.size(0), -1),
+        return feature1.view(feature1.size(0), -1),feature2.view(feature2.size(0), -1),feature3.view(feature3.size(0), -1),feature4.view(feature4.size(0), -1)
 
 
